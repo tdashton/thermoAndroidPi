5d92e100a647c8db8c67c7b570b4f52949a23447
Tyler <tdashton@gmail.com>
2015-08-23 13:45:45 +0200

    removed file, includes

--- app/src/main/java/ashtonandassociates/com/thermopi/ui/GraphActivity.java
+++ app/src/main/java/ashtonandassociates/com/thermopi/ui/GraphActivity.java
@@ -1,14 +1,9 @@
 package ashtonandassociates.com.thermopi.ui;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.android.volley.*;
