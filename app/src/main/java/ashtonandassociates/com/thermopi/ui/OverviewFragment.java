0bc5decd8ecabec01bd8f0b1c513db8fc95d4c77
Tyler <tdashton@gmail.com>
2015-10-16 11:00:58 +0200

    search bar stlye

--- app/src/main/java/ashtonandassociates/com/thermopi/ui/OverviewFragment.java
+++ app/src/main/java/ashtonandassociates/com/thermopi/ui/OverviewFragment.java
@@ -106,7 +106,7 @@ public class OverviewFragment extends Fragment {
 	@Override
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
 			// Inflate the menu; this adds items to the action bar if it is present.
-		getActivity().getMenuInflater().inflate(R.menu.menu_graph, menu);
+		getActivity().getMenuInflater().inflate(R.menu.menu_main, menu);
 	}
 
 	@Override
