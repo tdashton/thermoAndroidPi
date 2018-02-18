64cbc39d6c19fc08e129013b79bd2afddd5894f2
Tyler <tdashton@gmail.com>
2018-02-18 15:47:35 +0100

    add logic to expire nonce after 10 minutes

--- app/src/main/java/com/ashtonandassociates/thermopi/util/AppStateManager.java
+++ app/src/main/java/com/ashtonandassociates/thermopi/util/AppStateManager.java
@@ -1,5 +1,7 @@
 package com.ashtonandassociates.thermopi.util;
 
+import java.util.Date;
+
 public class AppStateManager {
 
 	private static AppStateManager instance = null;
@@ -9,15 +11,23 @@ public class AppStateManager {
 	public static AppStateManager getInstance() {
 		if(AppStateManager.instance == null) {
 			AppStateManager.instance = new AppStateManager();
+			AppStateManager.instance.apiNonceExpires = new Date(0);
 		}
 		return instance;
 	}
 
 	private String apiNonce = null;
+	private Date apiNonceExpires = null;
 	private String apiSharedSecret = null;
 
-	public void setApiNonce(String param) {
+	public void setApiNonce(String param, Date expires) {
+		this.apiNonceExpires = expires;
 		this.apiNonce = param;
+	}
+
+	public void setApiNonce(String param) {
+		long now = new Date().getTime();
+		this.setApiNonce(param, new Date(now + 10 * 60 * 1000));
 	}
 
 	public String getApiNonce() {
@@ -25,7 +35,8 @@ public class AppStateManager {
 	}
 
 	public boolean hasApiNonce() {
-		return this.apiNonce != null;
+		boolean expired = this.apiNonceExpires.before(new Date());
+		return this.apiNonce != null && !expired;
 	}
 
 	public String getApiSharedSecret() {
