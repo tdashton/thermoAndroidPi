e6ac626911d2eceb5096c4fa57c1a7476f9fe1c7
Tyler <tdashton@gmail.com>
2018-03-13 22:12:23 +0100

    cleanup

--- app/src/main/java/com/ashtonandassociates/thermopi/api/ApiListenerInterface.java
+++ app/src/main/java/com/ashtonandassociates/thermopi/api/ApiListenerInterface.java
@@ -1,19 +1,8 @@
 package com.ashtonandassociates.thermopi.api;
 
-import com.ashtonandassociates.thermopi.api.response.ControlCommandResponse;
-import com.ashtonandassociates.thermopi.api.response.ControlLogsResponse;
-import com.ashtonandassociates.thermopi.api.response.CurrentResponse;
-import com.ashtonandassociates.thermopi.api.response.HistoryResponse;
-import com.ashtonandassociates.thermopi.api.response.NonceResponse;
-
-import java.util.List;
-
-import retrofit.client.Response;
-
 /**
  * Created by theKernel on 13.03.2018.
  */
-
 public interface ApiListenerInterface {
 
 }
